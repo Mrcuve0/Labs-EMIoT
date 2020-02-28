326c80f9862e577d7347ee4a0b676aabd5dcdd43
Mrcuve0 <cerini.samuele@outlook.it>
2020-02-28 10:10:06 +0100

    Complete Report

--- Lab3/src/config.m
+++ Lab3/src/config.m
@@ -1,4 +1,4 @@
-load('gmonth.mat')
+load('gmonths.mat')
 
 % sensor activation durantion 
 air_time = 30; 
@@ -18,12 +18,12 @@ mc_time = 6;
 % transmit_delay = mc_delay + mc_time;
 
 % setting 2: serial exec - 588361 sec
-% air_delay = 0; % first to execute
-% methane_delay = air_time; % second to execute
-% temp_delay = methane_delay + methane_time; % third to execute
-% mic_delay = temp_delay + temp_time; % fourth to execute
-% mc_delay = mic_delay + mic_time; % fifth to execute
-% transmit_delay = mc_delay + mc_time; % sixth to execute
+air_delay = 0; % first to execute
+methane_delay = air_time; % second to execute
+temp_delay = methane_delay + methane_time; % third to execute
+mic_delay = temp_delay + temp_time; % fourth to execute
+mc_delay = mic_delay + mic_time; % fifth to execute
+transmit_delay = mc_delay + mc_time; % sixth to execute
 
 % setting 3: 588360 sec
 % air_delay = 0; % first to execute
@@ -34,12 +34,12 @@ mc_time = 6;
 % transmit_delay = mc_delay + mc_time; % sixth to execute
 
 % setting 4: 588291 sec
-methane_delay = 0; % first to execute
-air_delay = methane_time; % second to execute
-temp_delay = methane_time-mic_time-temp_time; % third to execute
-mic_delay = methane_time-mic_time; % fourth to execute
-mc_delay = air_time + air_time; % fifth to execute
-transmit_delay = mc_delay + mc_time; % sixth to execute
+% methane_delay = 0; % first to execute
+% air_delay = methane_time; % second to execute
+% temp_delay = methane_time-mic_time-temp_time; % third to execute
+% mic_delay = methane_time-mic_time; % fourth to execute
+% mc_delay = air_time + air_time; % fifth to execute
+% transmit_delay = mc_delay + mc_time; % sixth to execute
 
 % period
 % setting 1: 120 (2 minutes)
@@ -47,7 +47,7 @@ transmit_delay = mc_delay + mc_time; % sixth to execute
 period = 60*2; 
 
 % pulse width: % of period when sensor is active
-air_pulse = (air_time * 100)/period; 
+air_pulse = (air_time * 100)/(period*3); 
 methane_pulse = (methane_time * 100)/period; 
 temp_pulse = (temp_time *100)/period; 
 mic_pulse = (mic_time * 100)/period; 
@@ -56,6 +56,6 @@ transmit_pulse = (transmit_time * 100)/period;
 
 % simulation length
 % max simulation length = 1 month (30 days)
-length = 30*24*60*60; 
+length = 30*3*24*60*60; 
 
 
